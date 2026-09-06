9e3ca92daa6de78db6eaf1420ff28c9d72171303
udeudeude <udeudeude@gmail.com>
2026-09-05 22:11:33 -0400

    Add private Touch Bar API bridge

---
+++ TouchBarpalooza/TouchBarPrivateApi.h
@@ -0,0 +1,19 @@
+#import <AppKit/AppKit.h>
+
+extern void DFRElementSetControlStripPresenceForIdentifier(NSTouchBarItemIdentifier, BOOL);
+extern void DFRSystemModalShowsCloseBoxWhenFrontMost(BOOL);
+
+@interface NSTouchBarItem (TouchBarpaloozaPrivate)
++ (void)addSystemTrayItem:(NSTouchBarItem *)item;
++ (void)removeSystemTrayItem:(NSTouchBarItem *)item;
+@end
+
+@interface NSTouchBar (TouchBarpaloozaPrivate)
++ (void)presentSystemModalTouchBar:(NSTouchBar *)touchBar
+          systemTrayItemIdentifier:(NSTouchBarItemIdentifier)identifier;
++ (void)presentSystemModalTouchBar:(NSTouchBar *)touchBar
+                         placement:(long long)placement
+          systemTrayItemIdentifier:(NSTouchBarItemIdentifier)identifier;
++ (void)dismissSystemModalTouchBar:(NSTouchBar *)touchBar;
++ (void)minimizeSystemModalTouchBar:(NSTouchBar *)touchBar;
+@end
